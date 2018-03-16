4509dfe565373fb3c739c078dc721009f855cf92
Anduin Mooney <anduinmooney@gmail.com>
2018-03-16 16:02:40 -0700

    Add grid code to list activity, begin creating custom array adapter.

--- app/src/main/java/com/example/guest/app_foundation/ListActivity.java
+++ app/src/main/java/com/example/guest/app_foundation/ListActivity.java
@@ -1,8 +1,25 @@
 package com.example.guest.app_foundation;
 
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
 /**
  * Created by Anduin on 3/16/2018.
  */
 
-public class ListActivity {
+public class ListActivity extends AppCompatActivity {
+    GridView gridView;
+    String[] mockData = new String[] {"Daft Punk", "Meteor", "Massive Attack", "Deon Custom", "Mr FijiWiji", "Rogue", "Caravan Palace", "Madeon", "DotEXE", "BreakBot", "Com Truise"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridView = (GridView) findViewById(R.id.baseGridView);
+        gridView.setAdapter(new SpotifyAdapter(this, artists));
+    }
+
+
 }
---
+++ app/src/main/java/com/example/guest/app_foundation/SpotifyAdapter.java
@@ -0,0 +1,8 @@
+package com.example.guest.app_foundation;
+
+/**
+ * Created by Anduin on 3/16/2018.
+ */
+
+public class SpotifyAdapter {
+}
