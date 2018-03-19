761934d798f8bf6921d9e5e71026262d41aa60fc
Anduin Mooney <anduinmooney@gmail.com>
2018-03-18 17:50:21 -0700

    Add passing instrumentation test.

---
+++ app/src/test/java/com/example/guest/app_foundation/MainActivityTest.java
@@ -0,0 +1,28 @@
+package com.example.guest.app_foundation;
+
+import android.content.Intent;
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.Assert.assertTrue;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+}
