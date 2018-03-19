52e22581d91d149227fba0bc07bba1455ac40839
Anduin Mooney <anduinmooney@gmail.com>
2018-03-18 17:43:32 -0700

    Add and complete about and contact activity.

--- app/src/main/java/com/example/guest/app_foundation/ListActivity.java
+++ app/src/main/java/com/example/guest/app_foundation/ListActivity.java
@@ -1,19 +1,23 @@
 package com.example.guest.app_foundation;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListAdapter;
+import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Anduin on 3/16/2018.
  */
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.aboutButton) Button mAboutButton;
     @BindView(R.id.contactButton) Button mContactButton;
     GridView gridView;
@@ -26,9 +30,22 @@ public class ListActivity extends AppCompatActivity {
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/DEFTONE.ttf");
         gridView = (GridView) findViewById(R.id.baseGridView);
         gridView.setAdapter(new SpotifyAdapter(this, artists, typeface));
+        ButterKnife.bind(this);
 
         mAboutButton.setOnClickListener(this);
         mContactButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mAboutButton) {
+            Intent intent = new Intent(ListActivity.this, AboutActivity.class);
+            startActivity(intent);
+        } else if (v == mContactButton) {
+            Intent intent = new Intent(ListActivity.this, ContactActivity.class);
+            startActivity(intent);
+        }
+
     }
 
 
