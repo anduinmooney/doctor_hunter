647ef9b43807e0df71d39c8c35cb132e80ca4958
Anduin Mooney <anduinmooney@gmail.com>
2018-03-22 22:57:29 -0700

    Connect API key on build.

---
+++ app/src/main/java/com/example/guest/app_foundation/Constants.java
@@ -0,0 +1,9 @@
+package com.example.guest.app_foundation;
+
+/**
+ * Created by Anduin on 3/22/2018.
+ */
+
+public class Constants {
+    public static final String DOCTOR_TOKEN = BuildConfig.DOCTOR_TOKEN;
+}
