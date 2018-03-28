37d57d1fedbdcd4600400ac6bc077096fce064cc
Anduin Mooney <anduinmooney@gmail.com>
2018-03-28 14:51:45 -0700

    Set up firebase

--- app/src/main/java/com/example/guest/app_foundation/Constants.java
+++ app/src/main/java/com/example/guest/app_foundation/Constants.java
@@ -8,5 +8,6 @@ public class Constants {
     public static final String DOCTOR_TOKEN = BuildConfig.DOCTOR_TOKEN;
     public static final String DOCTOR_BASE_URL = ("https://api.betterdoctor.com/2016-03-01/doctors?" + "user_key=" + DOCTOR_TOKEN);
     public static final String DOCTOR_LOCATION_QUERY_PARAMETER = "location";
+    public static final String PREFERENCES_LOCATION_KEY = "location";
 
 }
