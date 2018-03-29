69aa6100c253b1bae32f99fd69d78a7de5d9c983
Anduin Mooney <anduinmooney@gmail.com>
2018-03-29 13:52:08 -0700

    Add temporary layout for login/signup pages

---
+++ app/src/main/java/com/example/guest/app_foundation/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.guest.app_foundation.ui;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.guest.app_foundation.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
