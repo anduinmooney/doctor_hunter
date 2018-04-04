1c4675a87ed103a67fe32020c2ad9f4b077ddcb8
Anduin Mooney <anduinmooney@gmail.com>
2018-04-04 09:41:10 -0700

    Create OnStartDragListener & SimpleItemTouchHelperCallback

---
+++ app/src/main/java/com/example/guest/app_foundation/util/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package com.example.guest.app_foundation.util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Guest on 4/4/18.
+ */
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
---
+++ app/src/main/java/com/example/guest/app_foundation/util/SimpleItemTouchHelperCallback.java
@@ -0,0 +1,8 @@
+package com.example.guest.app_foundation.util;
+
+/**
+ * Created by Guest on 4/4/18.
+ */
+
+public class SimpleItemTouchHelperCallback {
+}
