4e4b5aa58654936202866fc645bfb348dbd540d5
Anduin Mooney <anduinmooney@gmail.com>
2018-04-04 11:18:11 -0700

    Refactor SavedDoctorListAcitvity to implement FirebaseDoctorListAdapter

--- app/src/main/java/com/example/guest/app_foundation/ui/SavedDoctorListActivity.java
+++ app/src/main/java/com/example/guest/app_foundation/ui/SavedDoctorListActivity.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import com.example.guest.app_foundation.Constants;
 import com.example.guest.app_foundation.R;
+import com.example.guest.app_foundation.adapter.FirebaseDoctorListAdapter;
 import com.example.guest.app_foundation.adapter.FirebaseDoctorViewHolder;
 import com.example.guest.app_foundation.models.Doctor;
+import com.example.guest.app_foundation.util.OnStartDragListener;
+import com.example.guest.app_foundation.util.SimpleItemTouchHelperCallback;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,12 +22,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SavedDoctorListActivity extends AppCompatActivity {
+public class SavedDoctorListActivity extends AppCompatActivity implements OnStartDragListener {
     private DatabaseReference mDoctorReference;
     private FirebaseRecyclerAdapter mFirebaseAdapter;
+    private ItemTouchHelper mItemTouchHelper;
 
-    @BindView(R.id.recyclerView)
-    RecyclerView mRecyclerView;
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,10 @@ public class SavedDoctorListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_doctor);
         ButterKnife.bind(this);
 
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = user.getUid();
 
@@ -39,29 +47,27 @@ public class SavedDoctorListActivity extends AppCompatActivity {
                 .getInstance()
                 .getReference(Constants.FIREBASE_CHILD_DOCTORS)
                 .child(uid);
-        
-        setUpFirebaseAdapter();
-    }
 
-    private void setUpFirebaseAdapter() {
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<Doctor, FirebaseDoctorViewHolder>
-                (Doctor.class, R.layout.doctor_list_layout_drag, FirebaseDoctorViewHolder.class,
-                        mDoctorReference) {
+        mFirebaseAdapter = new FirebaseDoctorListAdapter(Doctor.class, R.layout.doctor_list_layout_drag, FirebaseDoctorViewHolder.class, mDoctorReference, this, this);
 
-            @Override
-            protected void populateViewHolder(FirebaseDoctorViewHolder viewHolder,
-                                              Doctor model, int position) {
-                viewHolder.bindDoctor(model);
-            }
-        };
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mFirebaseAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
+
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mFirebaseAdapter.cleanup();
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
     }
 }
