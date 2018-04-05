ccb79d480f0eb8ca397e166c29be208875fa3a18
Anduin Mooney <anduinmooney@gmail.com>
2018-04-05 10:05:19 -0700

    Remove & Comment out code unneeded code from DoctorActivity

--- app/src/main/java/com/example/guest/app_foundation/ui/DoctorActivity.java
+++ app/src/main/java/com/example/guest/app_foundation/ui/DoctorActivity.java
@@ -44,59 +44,28 @@ import okhttp3.Response;
  * Created by Anduin on 3/16/2018.
  */
 
-public class DoctorActivity extends AppCompatActivity implements View.OnClickListener {
-    @BindView(R.id.aboutButton) Button mAboutButton;
-    @BindView(R.id.contactButton) Button mContactButton;
-    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
-    private DoctorListAdapter mAdapter;
-
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
-    private String mRecentAddress;
-
-    public static final String TAG = DoctorActivity.class.getSimpleName();
-    public ArrayList<Doctor> doctors = new ArrayList<>();
-
-
+public class DoctorActivity extends AppCompatActivity {
+//    @BindView(R.id.aboutButton) Button mAboutButton;
+//    @BindView(R.id.contactButton) Button mContactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/DEFTONE.ttf");
-        ButterKnife.bind(this);
+//        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/DEFTONE.ttf");
+//        mAboutButton.setOnClickListener(this);
+//        mContactButton.setOnClickListener(this);
+    }
 
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
-//        if (mRecentAddress != null) {
-//            getDoctors(mRecentAddress);
+//    @Override
+//    public void onClick(View v) {
+//        if (v == mAboutButton) {
+//            Intent intent = new Intent(DoctorActivity.this, AboutActivity.class);
+//            startActivity(intent);
+//        } else if (v == mContactButton) {
+//            Intent intent = new Intent(DoctorActivity.this, ContactActivity.class);
+//            startActivity(intent);
 //        }
-
-        mAboutButton.setOnClickListener(this);
-        mContactButton.setOnClickListener(this);
-        Intent intent = getIntent();
-        String location = intent.getStringExtra("location");
-        String query = intent.getStringExtra("query");
-
-//        getDoctors(location);
-//        getDoctors(query);
-
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == mAboutButton) {
-            Intent intent = new Intent(DoctorActivity.this, AboutActivity.class);
-            startActivity(intent);
-        } else if (v == mContactButton) {
-            Intent intent = new Intent(DoctorActivity.this, ContactActivity.class);
-            startActivity(intent);
-        }
-
-    }
-
-
-
-
+//
+//    }
 }
