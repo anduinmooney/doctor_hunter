a2bc54b93a5cb5db0695f26e0fc63d2c06de66a3
Anduin Mooney <anduinmooney@gmail.com>
2018-04-05 09:41:56 -0700

    Override onCreate method in DoctorListFragment

--- app/src/main/java/com/example/guest/app_foundation/ui/DoctorListFragment.java
+++ app/src/main/java/com/example/guest/app_foundation/ui/DoctorListFragment.java
@@ -1,29 +1,56 @@
 package com.example.guest.app_foundation.ui;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.guest.app_foundation.R;
+import com.example.guest.app_foundation.adapter.DoctorListAdapter;
+import com.example.guest.app_foundation.models.Doctor;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DoctorListFragment extends Fragment {
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    private DoctorListAdapter mAdapter;
+    public ArrayList<Doctor> mDoctors = new ArrayList<>();
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentAddress;
 
 
     public DoctorListFragment() {
-        // Required empty public constructor
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mEditor = mSharedPreferences.edit();
+
+        // Instructs fragment to include menu options:
+        setHasOptionsMenu(true);
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_doctor_list, container, false);
     }
 
