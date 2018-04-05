bc28edc1d5863cf3ec7cf2e0c69e84a7b90a6405
Anduin Mooney <anduinmooney@gmail.com>
2018-04-05 10:07:39 -0700

    Create SavedDoctorListFragment

---
+++ app/src/main/java/com/example/guest/app_foundation/ui/SavedDoctorListFragment.java
@@ -0,0 +1,41 @@
+package com.example.guest.app_foundation.ui;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.guest.app_foundation.R;
+import com.example.guest.app_foundation.adapter.FirebaseDoctorListAdapter;
+import com.example.guest.app_foundation.util.OnStartDragListener;
+
+import butterknife.BindView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SavedDoctorListFragment extends Fragment implements OnStartDragListener {
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    private FirebaseDoctorListAdapter mFirebaseAdapter;
+    private ItemTouchHelper mItemTouchHelper;
+
+
+    public SavedDoctorListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved_doctor_list, container, false);
+    }
+
+}
