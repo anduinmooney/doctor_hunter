915297de934a98202ec7e26b9555e106a0a13ac4
Student <example@example.com>
2018-04-05 08:32:43 -0700

    Create ItemTouchHelperViewHolder class

---
+++ app/src/main/java/com/example/guest/app_foundation/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,10 @@
+package com.example.guest.app_foundation.util;
+
+/**
+ * Created by Guest on 4/5/18.
+ */
+
+public interface ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
