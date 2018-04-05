e30d267849371f4abc5dd9693f508a1aa83340da
Anduin Mooney <anduinmooney@gmail.com>
2018-04-05 10:13:39 -0700

    Move setUpFirebaseAdapter, onDestroy, & onStartDrag from SavedDoctorListActivity to fragment

--- app/src/main/java/com/example/guest/app_foundation/ui/SavedDoctorListFragment.java
+++ app/src/main/java/com/example/guest/app_foundation/ui/SavedDoctorListFragment.java
@@ -3,22 +3,29 @@ package com.example.guest.app_foundation.ui;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.guest.app_foundation.Constants;
 import com.example.guest.app_foundation.R;
 import com.example.guest.app_foundation.adapter.FirebaseDoctorListAdapter;
+import com.example.guest.app_foundation.adapter.FirebaseDoctorViewHolder;
+import com.example.guest.app_foundation.models.Doctor;
 import com.example.guest.app_foundation.util.OnStartDragListener;
+import com.example.guest.app_foundation.util.SimpleItemTouchHelperCallback;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class SavedDoctorListFragment extends Fragment implements OnStartDragListener {
 
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
@@ -28,7 +35,7 @@ public class SavedDoctorListFragment extends Fragment implements OnStartDragList
 
 
     public SavedDoctorListFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -39,6 +46,41 @@ public class SavedDoctorListFragment extends Fragment implements OnStartDragList
         ButterKnife.bind(this, view);
         setUpFirebaseAdapter();
         return view;
+    }
+
+    private void setUpFirebaseAdapter() {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        Query query = FirebaseDatabase.getInstance()
+                .getReference(Constants.FIREBASE_CHILD_DOCTORS)
+                .child(uid)
+                .orderByChild(Constants.FIREBASE_QUERY_INDEX);
+
+
+        mFirebaseAdapter = new FirebaseDoctorListAdapter(Doctor.class,
+                R.layout.doctor_list_layout_drag, FirebaseDoctorViewHolder.class,
+                query, this, getActivity());
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
     }
 
 }
